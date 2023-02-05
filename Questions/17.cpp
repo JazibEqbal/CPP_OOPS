b662ed7ece1cdfd4f0f074592c74085bde6f4915
JazibEqbal <jazibeqbal0526@gmail.com>
2023-02-05 18:48:08 +0530

    17

---
+++ Questions/17.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+class Array{
+    int size;
+    int *arr;
+public:
+    Array(){
+        size = 5;
+        arr= new int[size];
+        for(int i=0; i<size;i++){
+            arr[i] =0;
+        }
+    }
+    Array(int size){
+        this->size=size;
+        arr = new int[size];
+        for(int i=0; i<size;i++){
+            arr[i] =0;
+        }
+    }
+    //Copy constructor
+    Array(Array& x){
+        size=x.size;
+    }
+    ~Array(){
+        delete[] arr;
+    }
+};
+
+int main(){
+    int size;
+    cin>>size;
+    Array a(size);
+    Array b(a);
+    return 0;
+}
